b0f912c876e7a4312a160bb0cee8c9cc45aa3c05
arhamh7 <90596316+arhamh7@users.noreply.github.com>
2023-03-21 10:12:37 -0700

    Update BST.java

--- Assignment3/src/BST.java
+++ Assignment3/src/BST.java
@@ -1,229 +1,172 @@
-
-
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
-import java.util.Queue;
+import java.util.Stack;
 
-public class BST<T extends Comparable<T>> implements Iterable<T> {
-	class BSTNode implements Comparable<BSTNode> {
-		private T data;
-		private BSTNode left;
-		private BSTNode right;
+public class BST<T extends Comparable<T>> {
 
-		public BSTNode(T d) {
-			setLeft(null);
-			setRight(null);
-			setData(d);
-		}
-
-		public T getData() {
-			return data;
-		}
-
-		public void setData(T data) {
-			this.data = data;
-		}
-
-		public BSTNode getLeft() {
-			return left;
-		}
-
-		public void setLeft(BSTNode left) {
-			this.left = left;
-		}
-
-		public BSTNode getRight() {
-			return right;
-		}
-
-		public void setRight(BSTNode right) {
-			this.right = right;
-		}
-
-		public boolean isLeaf() {
-			return (getLeft() == null) && (getRight() == null);
-		}
-
-		@Override
-		public int compareTo(BST<T>.BSTNode o) {
-			return this.getData().compareTo(o.getData());
-		}
-
-		public void isEmpty() {
-			left = null;
-			right = null;
-			size = 0;
-
-		}
-	}
-
-	private static final int INORDER = 0;
-	private static final int PREORDER = 1;
-	private static final int POSTORDER = 1;
-
-	private BSTNode root;
-	private int size;
+	private Node<T> root;
 	private Comparator<T> comparator;
-	private Queue<T> queue = new LinkedList<T>();
-	
+
 	public BST() {
 		root = null;
-		size = 0;
-		comparator = null;
-	}
-	
-	public BST(Comparator<T> externalComp) {
-		root = null;
-		size = 0;
-		comparator = externalComp;
 	}
 
-	public int size() {
-		return size;
+	public BST(Comparator<T> comparator) {
+		root = null;
+		this.comparator = comparator;
 	}
-	
-	public void deleteNode(T key) {
-        root = deleteNode(root, key);
+
+	private static class Node<T> {
+		T value;
+		Node<T> left, right;
+
+		Node(T value) {
+			this.value = value;
+		}
+	}
+
+	public void insert(T value) {
+		root = insertRecursively(root, value);
+		
+	}
+
+	private Node<T> insertRecursively(Node<T> node, T value) {
+
+		if (node == null) {
+			node = new Node<T>(value);
+			return node;
+		}
+		
+		if ( compare(value, node.value) < 0) {
+			
+			node.left = insertRecursively(node.left, value);
+		} else if (compare(value, node.value) > 0) {
+			node.right = insertRecursively(node.right, value);
+		}
+
+		return node;
+	}
+
+	public void remove(T value) {
+        root = remove(root, value);
     }
 
-	private int height(BSTNode r) {
-		int h = -1;
-		if (r == null) {
-			return h;
-		} else {
-			int leftHeight = height(r.getLeft());
-			int rightHeight = height(r.getRight());
-			h = Math.max(leftHeight, rightHeight) + 1;
-		}
-		return h;
-	}
-
-	public int height() {
-		return height(root);
-	}
-
-	public void add(T d) {
-		BSTNode n = new BSTNode(d);
-		if (root == null) {
-			size++;
-			root = n;
-		} else {
-			add(root, n);
-		}
-	}
-
-	private void add(BSTNode r, BSTNode n) {
-		int c = n.compareTo(r);
-		if (c < 0) {
-			if (r.getLeft() == null) {
-				r.setLeft(n);
-			} else
-				add(r.getLeft(), n);
-		} else {
-			if (r.getRight() == null)
-				r.setRight(n);
-			else {
-				add(r.getRight(), n);
-			}
-		}
-	}
-	
-	private BSTNode deleteNode(BSTNode node, T key) {
+    private Node<T> remove(Node<T> node, T value) {
         if (node == null) {
             return null;
         }
 
-        int cmp = key.compareTo(node.data);
-        if (cmp < 0) {
-            node.left = deleteNode(node.left, key);
-        } else if (cmp > 0) {
-            node.right = deleteNode(node.right, key);
+        if (compare(value, node.value) < 0) {
+            node.left = remove(node.left, value);
+        } else if (compare(value, node.value) > 0) {
+            node.right = remove(node.right, value);
         } else {
-            if (node.left == null) {
-                return node.right;
+            if (node.left == null && node.right == null) {
+                node = null;
+            } else if (node.left == null) {
+                node = node.right;
             } else if (node.right == null) {
-                return node.left;
+                node = node.left;
             } else {
-                BSTNode temp = node.right;
-                while (temp.left != null) {
-                    temp = temp.left;
-                }
-                node.data = temp.data;
-                node.right = deleteNode(node.right, temp.data);
+                Node<T> minRight = findMin(node.right);
+                node.value = minRight.value;
+                node.right = remove(node.right, minRight.value);
             }
         }
 
         return node;
     }
 
-	private void visit(BSTNode r) {
-		if (r != null)
-			queue.add(r.getData());
-	}
+    private Node<T> findMin(Node<T> node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+	
+	
+    public boolean search(T value) {
+        return searchRecursively(root, value);
+    }
 
-	private void traverseInOrder(BSTNode r) {
-		if (r != null)
-			return;
-		else {
-			traverseInOrder(r.getLeft());
-			visit(r);
-			traverseInOrder(r.getRight());
-		}
-	}
+    private boolean searchRecursively(Node<T> root, T value) {
+        if (root == null) {
+            return false;
+        }
 
-	private void traversePreOrder(BSTNode r) {
-		if (r != null)
-			return;
-		else {
-			visit(r);
-			traversePreOrder(r.getLeft());
-			traversePreOrder(r.getRight());
+        if (root.value.equals(value)) {
+            return true;
+        }
+
+        if (compare(value, root.value) < 0) {
+            return searchRecursively(root.left, value);
+        } else {
+            return searchRecursively(root.right, value);
+        }
+    }
+    
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node<T> node) {
+        if (node == null) {
+            return -1;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+    
+    int size(Node<T> node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return 1 + size(node.left) + size(node.right);
+        }
+    }
+
+    int size() {
+        return size(root);
+    }
+
+	
+	private int compare(T o1, T o2) {
+		if (comparator != null) {
+			return comparator.compare(o1, o2);
+		
 		}
+		return o1.compareTo(o2);
 	}
 	
-	private void traversePostOrder(BSTNode r) {
-		if(r != null)
-			return;
-		else {
-			traversePostOrder(r.getLeft());
-			traversePostOrder(r.getRight());
-			visit(r);
-		}
-	}
+	private class InorderIterator implements Iterator<T>{
+        private Stack<Node<T>> stack;
 
-	private void traverse(BSTNode r, int travType) {
-		if (travType == INORDER) {
-			traverseInOrder(r);
-		} else if (travType == PREORDER) {
-			traversePreOrder(r);
-		} else if (travType == POSTORDER) {
-			traversePostOrder(r);
-		}
-	}
+        public InorderIterator() {
+            stack = new Stack<>();
+            pushLeftNodes(root);
+        }
 
-	private class BSTIterator implements Iterator {
-		public BSTIterator() {
-				queue.isEmpty();
-				traverse(root, INORDER);
-			}
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
 
-		@Override
-		public boolean hasNext() {
-			return !queue.isEmpty();
-		}
+        public T next() {
+            Node<T> node = stack.pop();
+            pushLeftNodes(node.right);
+            return node.value;
+        }
 
-		@Override
-		public T next() {
-			return queue.remove();
-		}
-	}
+        private void pushLeftNodes(Node<T> node) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+    }
 
-	@Override
-	public Iterator<T> iterator() {
-		Iterator it = new BSTIterator();
-		return  it;
-	}
+    public Iterator<T> iterator() {
+        return new InorderIterator();
+    }
 
 }
