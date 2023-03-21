667d5b61a68a3552b3028776a4b7661e946ca7d3
arhamh7 <90596316+arhamh7@users.noreply.github.com>
2023-03-21 10:13:24 -0700

    Update AvengerComparatorFreqDescending.java

--- Assignment3/src/AvengerComparatorFreqDescending.java
+++ Assignment3/src/AvengerComparatorFreqDescending.java
@@ -1,30 +1,18 @@
 import java.util.Comparator;
 
-public class AvengerComparatorFreqDescending implements Comparator<Avenger> {
+public class AvengerComparatorFreqDescending implements Comparator<Avenger>{
 
-	/**
-	 * override compare method for sorting the least popular avenger
-	 */
-	
 	@Override
-	public int compare(Avenger a, Avenger b) {
-		int freq1 = a.getFrequency();
-		int freq2 = b.getFrequency();
+	public int compare(Avenger o1, Avenger o2) {
 		
-		//if else statement that catches if the value of a - b is 0, 1, -1
-		if(freq1-freq2 == 0) {
-			int result;
-			//if two objects have same frequency compare the length of word
-			if(a.getHeroName().length() < b.getHeroName().length()) {
-				result = b.compareTo(a);
-			} else if (a.getHeroName().length() == b.getHeroName().length()){
-				result = a.getHeroName().compareTo(b.getHeroName());
-			} else {
-				result = a.compareTo(b);
-			}
-			return result;
+		if(o1.getFrequency() > o2.getFrequency()) {
+			return -1;
 		}
-		return freq1-freq2;
+		if(o1.getFrequency() < o2.getFrequency()) {
+			return 1;
+		}
+		return o1.compareTo(o2);
+		
 	}
-
+	
 }
