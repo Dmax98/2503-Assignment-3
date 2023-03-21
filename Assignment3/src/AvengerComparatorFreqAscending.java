b6734b8243bbbd2de5f7e6f483109c412448af79
arhamh7 <90596316+arhamh7@users.noreply.github.com>
2023-03-21 10:14:09 -0700

    Update AvengerComparatorFreqAscending.java

--- Assignment3/src/AvengerComparatorFreqAscending.java
+++ Assignment3/src/AvengerComparatorFreqAscending.java
@@ -1,18 +1,20 @@
 import java.util.Comparator;
 
-public class AvengerComparatorFreqDescending implements Comparator<Avenger>{
+public class AvengerComparatorFreqAscending implements Comparator<Avenger>{
 
 	@Override
 	public int compare(Avenger o1, Avenger o2) {
-		
 		if(o1.getFrequency() > o2.getFrequency()) {
+			return 1;
+		}else if(o1.getFrequency() < o2.getFrequency()) {
 			return -1;
 		}
-		if(o1.getFrequency() < o2.getFrequency()) {
-			return 1;
-		}
-		return o1.compareTo(o2);
 		
+		 int result = o1.getHeroAlias().length() - o2.getHeroAlias().length();
+	        if(result == 0){
+	            return o1.getHeroAlias().compareTo(o2.getHeroAlias());
+	        }
+	        return result;
 	}
-	
+
 }
