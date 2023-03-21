6dc685c939637360626fcc62bc8747c36ba2044a
arhamh7 <90596316+arhamh7@users.noreply.github.com>
2023-03-21 10:13:48 -0700

    Update AvengerComparatorFreqAscending.java

--- Assignment3/src/AvengerComparatorFreqAscending.java
+++ Assignment3/src/AvengerComparatorFreqAscending.java
@@ -1,21 +1,18 @@
 import java.util.Comparator;
 
-public class AvengerComparatorFreqAscending implements Comparator<Avenger> {
-	
+public class AvengerComparatorFreqDescending implements Comparator<Avenger>{
+
 	@Override
-	/**
-	 * Total order:
-	 * ascending order of total frequency 
-	 * in case of tie, in ascending order of last name length
-	 * in case of tie, in ascending alphabetical order of last name
-	 */
-	public int compare(Avenger a1, Avenger a2) {
-		int diff = a1.getFrequency() - a2.getFrequency();
-		if (diff == 0) {
-			diff = a1.getHeroName().length() - a2.getHeroName().length();
-			if (diff == 0)
-				return a1.getHeroName().compareTo(a2.getHeroName());
+	public int compare(Avenger o1, Avenger o2) {
+		
+		if(o1.getFrequency() > o2.getFrequency()) {
+			return -1;
 		}
-		return diff;
+		if(o1.getFrequency() < o2.getFrequency()) {
+			return 1;
+		}
+		return o1.compareTo(o2);
+		
 	}
+	
 }
